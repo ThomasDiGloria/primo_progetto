b2d5886bed1bb2aa7c8fc0e4160d515ff64eda72
ThomasDiGloria <thomasdigloria@gmail.com>
2024-11-27 11:56:18 +0100

    NOn va "if_esle_elif"

--- seconda_app/urls.py
+++ seconda_app/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from seconda_app.views import es_if
+from seconda_app.views import es_if, if_esle_elif
 
 app_name="seconda_app"
 
 urlpatterns = [
      path('es_if',es_if,name='es_if'),
+     path('if_esle_elif',if_esle_elif,name='if_esle_elif'),
+     
 ]
--- seconda_app/views.py
+++ seconda_app/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 
 def es_if(request):
-        context = {
-            'var1' : 200,
-            'var2' : 200,
-            'var3' : 300,
-        }
-        return render(request, "es_if.html", context)
+    context = {
+        'var1' : 200,
+        'var2' : 200,
+        'var3' : 300,
+    }
+    return render(request, "es_if.html", context)
+
+def if_esle_elif(request):
+    context = {
+        'var1' : 100,
+        'var2' : 100.0,
+        'var3' : 100.50,
+    }
+    return render(request, "if_esle_elif.html", context)
