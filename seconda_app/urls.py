0751054b2db60e8e3652eac8bdb40908aefdd8c6
ThomasDiGloria <thomasdigloria@gmail.com>
2024-12-04 10:14:13 +0100

    Aggiornamento passo 12

--- seconda_app/urls.py
+++ seconda_app/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from seconda_app.views import es_if, if_esle_elif
+from seconda_app.views import es_if, if_esle_elif, es_for
 
 app_name="seconda_app"
 
 urlpatterns = [
      path('es_if',es_if,name='es_if'),
      path('if_esle_elif',if_esle_elif,name='if_esle_elif'),
-     
+     path('es_for', es_for,name='es_for')
 ]
