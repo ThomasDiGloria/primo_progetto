2c647e213266d8788a9b2675004515838f43cae1
ThomasDiGloria <thomasdigloria@gmail.com>
2024-12-04 11:52:55 +0100

    da finire passo 13

--- seconda_app/urls.py
+++ seconda_app/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from seconda_app.views import es_if, if_esle_elif, es_for
+from seconda_app.views import es_if, if_esle_elif, es_for, base
 
 app_name="seconda_app"
 
 urlpatterns = [
      path('es_if',es_if,name='es_if'),
      path('if_esle_elif',if_esle_elif,name='if_esle_elif'),
-     path('es_for', es_for,name='es_for')
+     path('es_for', es_for,name='es_for'),
+     
 ]
