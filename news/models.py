d3788e5d9c94e27f5d7b3924ee67b11430c98c74
ThomasDiGloria <thomasdigloria@gmail.com>
2025-01-15 11:10:17 +0100

    passo 17

--- news/models.py
+++ news/models.py
@@ -6,6 +6,10 @@ class Giornalista(models.Model):
 
     def __str__(self):
         return self.nome + " " + self.cognome
+    
+    class Meta:
+        verbose_name = "Giornalista"
+        verbose_name_plural = "Giornalisti"
 
 class Articolo(models.Model):
     """ il modello generico di un articolo di news """
@@ -16,3 +20,6 @@ class Articolo(models.Model):
     def __str__(self):
         return self.titolo
 
+    class Meta:
+        verbose_name = "Articolo"
+        verbose_name_plural = "Articoli"
