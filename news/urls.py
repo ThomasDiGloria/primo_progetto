2971f3b85afdaf10443b893fe179d71d7e745821
ThomasDiGloria <thomasdigloria@gmail.com>
2025-01-15 11:51:01 +0100

    quasi passo 18, devo correggere

--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import home, articoloDetailView, listaArticoli
+#query_base
 
 app_name = 'news'
 
@@ -8,4 +9,5 @@ urlpatterns = [
     path("articoli/<int:pk>/", articoloDetailView, name="articolo_detail"),
     path("lista_articoli/<int:pk>/", listaArticoli, name="lista_articoli"),
     path("lista_articoli/", listaArticoli, name="lista_articoli"),
+    #path('query', query_base, name = "query")
 ]
