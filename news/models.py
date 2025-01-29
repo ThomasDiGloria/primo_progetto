534f9e710b2f4222e18517f50525f4b2df2dd17e
ThomasDiGloria <thomasdigloria@gmail.com>
2025-01-29 11:20:16 +0100

    query sistemata (quasi)

--- news/models.py
+++ news/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from datetime import datetime
 
 class Giornalista(models.Model):
     nome = models.CharField(max_length=20)
     cognome = models.CharField(max_length=20)
+    anno_di_nascita = models.DateField(max_length=10, default=datetime.now(), blank=True)
 
     def __str__(self):
         return self.nome + " " + self.cognome
@@ -16,7 +18,8 @@ class Articolo(models.Model):
     titolo = models.CharField(max_length=100) #alcuni capi necessitano di parametri obbligatori
     contenuto = models.TextField()
     giornalista = models.ForeignKey(Giornalista, on_delete=models.CASCADE, related_name="articoli")
-    visualizzazioni = models.IntegerField(default=0) 
+    visualizzazioni = models.IntegerField(blank=True, default=0)
+    data = models.DateField(default=datetime.now(), blank=True)
 
     def __str__(self):
         return self.titolo
