9e45a6da97dd2cf615c9065454de670db2431377
ThomasDiGloria <thomasdigloria@gmail.com>
2025-02-18 16:32:44 +0100

    admin

--- corsi_formazione/models.py
+++ corsi_formazione/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Corso(models.Model):
+    titolo = models.CharField(100)
+    descrizione = models.TextField
+    data_inizio = models.DateField(default=datetime.now(), blank=True)
+    data_fine = models.DateField(default=datetime.now(), blank=True)
+    posti_disponibili = models.IntegerField(blank=True, default=0)
+    
+    def __str__(self):
+        return self.titolo + " " + self.descrizione + " " + self.data_inizio + " " + self.data_fine + " " + self.posti_disponibili
+    
+    '''class Meta:
+        verbose_name = "Giornalista"
+        verbose_name_plural = "Giornalisti"
+        '''
