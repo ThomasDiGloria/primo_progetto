f8a007807738d50e07b20f2b494a30a56c8dd0af
ThomasDiGloria <149668933+ThomasDiGloria@users.noreply.github.com>
2025-04-30 11:37:24 +0200

    spotify

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import todos_view
+from .views import todos_view, test_spotify
 
 app_name = 'api'
 
 urlpatterns = [
     path('todos', todos_view, name = 'todos'),
+    path('test_spotify', test_spotify, name = 'test_spotify'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
 import requests
 from django.shortcuts import render
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 def todos_view(request):
     try: 
@@ -20,3 +22,40 @@ def todos_view(request):
         'todos': lista_todos,
         'errore': messaggio_errore
     })
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="dca7084a57e44116926d2d93388509e4",
+                                                client_secret="aa9ebc8cf6614bc78546cddcf2b764ad",
+                                                #redirect_uri="http://localhost:8888/callback",
+                                                redirect_uri="http://127.0.0.1:8888/callback",
+                                                scope=["user-library-read"],
+                                                cache_path=".cache"))
+
+# Ottenere informazioni su un brano specifico
+'''
+    track = sp.track('spotify_track_id')
+    print(track)
+    album = sp.album('spotify_album_id')
+    print(album)
+'''
+
+def test_spotify(request):
+    results = sp.current_user_saved_tracks()
+    risultati = {
+
+    }
+    lista_canzoni = []
+    img = []
+    for idx, item in enumerate(results['items']):
+        track_name = item['track']['name']
+        track = item['track']
+        img.append(item['track']['album']['images'][0]['url'])
+        lista_canzoni.append(track_name)
+        #print(f"{track['name']} - {track['artists'][0]['name']}")
+    context = {
+        "results": lista_canzoni,
+        "track": track,
+        "img": img,
+        
+    }    
+    return render(request, 'spotify.html', context)
+    
